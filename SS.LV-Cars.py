d645f403d101682f94d5571c3143e8db0298bd4d
Dmitrijs Livinskis <dlivinskis@gmail.com>
2023-05-29 13:16:43 +0300

    Added functionality for emails in the main scrapping file for cars

--- SS.LV-Cars.py
+++ SS.LV-Cars.py
@@ -6,6 +6,7 @@ from datetime import date
 import time
 import pandas as pd
 from html_table_parser.parser import HTMLTableParser
+from sending_email import send_email
 
 today = date.today()
 
@@ -14,21 +15,22 @@ def url_get_contents(url):
     req = urllib.request.Request(url=url)
     f = urllib.request.urlopen(req)
     return f.read()
-car_list_1 = ['alfa-romeo','chevrolet','chrysler','dacia','dodge','fiat','infiniti','jaguar','jeep','land-rover','mercedes','mini','mitsubishi','porsche','saab','seat','smart','subaru','suzuki','vaz'
+car_list_1 = ['chevrolet','chrysler','dacia','dodge','fiat','infiniti','jaguar','jeep','mercedes','mini','mitsubishi','porsche','saab','smart','subaru','suzuki','vaz','alfa-romeo','seat','lexus','kia'
 ]
 car_list_2 = ['audi','citroen','ford','hyundai','nissan','opel','peugeot','renault','toyota','volvo','mazda','kia'
 ]
-car_list_3 = ['honda','skoda','volkswagen','lexus'
+car_list_3 = ['honda','volkswagen','skoda','land-rover','lexus'
 ]
 car_list_4 = ['others']
 car_list_5 = ['bmw']
-# car_list_6 = ['lexus']
-# car_list_7 = ['kia']
+# car_list_6 = ['seat']
+# car_list_7 = []
 
 ExcelName = str(today) + ".xlsx"
 list1 = []
 list2 = []
 ldf = []
+list_with_errors = []
 def Get_Number_Of_Ads():
     response=requests.get('https://www.ss.com/en/transport/cars')
     print(response.text)
@@ -37,30 +39,36 @@ def Get_Number_Of_Ads():
     # for h4 in h4_tags:
     #     span_tags = h4.find_all('span', recursive=False)
     #     print(span_tags)
+
 def Getting_Cars(List_Of_Car_Brands,Table_Number):
     df2 = pd.DataFrame()
     Number_Of_Pages = 200
     for car in List_Of_Car_Brands:
+
         for x in range(1, Number_Of_Pages):
-            # time.sleep(0.1)
-            xhtml = url_get_contents(f"https://www.ss.com/en/transport/cars/{car}/page{x}.html").decode('utf-8')
-            p = HTMLTableParser()
-            p.feed(xhtml)
-            df1 = pd.DataFrame(p.tables[Table_Number])
+            try:
+                # time.sleep(0.1)
+                xhtml = url_get_contents(f"https://www.ss.com/en/transport/cars/{car}/page{x}.html").decode('utf-8')
+                p = HTMLTableParser()
+                p.feed(xhtml)
+                df1 = pd.DataFrame(p.tables[Table_Number])
 
-            print(car)
-            # print(df1)
-            df1.rename(
-                columns={0: 'nothing', 1: 'nothing1', 2: 'Text', 3: 'Model', 4: 'Year', 5: 'Engine', 6: 'Mileage',
-                         7: 'Price'}, inplace=True)
+                print(car)
+                # print(df1)
+                df1.rename(
+                    columns={0: 'nothing', 1: 'nothing1', 2: 'Text', 3: 'Model', 4: 'Year', 5: 'Engine', 6: 'Mileage',
+                             7: 'Price'}, inplace=True)
 
-            df1.insert(loc=8,
-                       column='Date',
-                       value=today)
-            df1.insert(loc=9,
-                       column='Car Section',
-                       value=car)
-
+                df1.insert(loc=8,
+                           column='Date',
+                           value=today)
+                df1.insert(loc=9,
+                           column='Car Section',
+                           value=car)
+            except:
+                print(car + ' is wrong')
+                error=(car + ' is wrong')
+                list_with_errors.append(error)
 
             try:
                 df1.drop('nothing', inplace=True, axis=1)
@@ -94,6 +102,10 @@ Getting_Cars(car_list_5,3)
 # Getting_Cars(car_list_6,5)
 pd.concat(ldf).to_excel(ExcelName)
 
+print(list_with_errors)
+send_email('done\n' + str(list_with_errors))
 
 time.sleep(5)
 shutil.move(f"W:\\Coding/PythonProjects\\SS.LV_Scrapping\\{ExcelName}",f"W:\\Coding\\PythonProjects\\ScrappedData\\OneDrive\\Cars")
+
+
