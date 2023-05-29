fd9c894dae6272f5382bd2797a177afd8229162f
Dmitrijs Livinskis <dlivinskis@gmail.com>
2023-05-29 18:12:26 +0300

    Added email notifications if all files were created

--- Check_if_files_exist.py
+++ Check_if_files_exist.py
@@ -1,16 +1,25 @@
 from datetime import date
 import time
 import os
+import pandas as pd
+from sending_email import send_email
 
 today = date.today()
 ExcelName = str(today) + ".xlsx"
+df = pd.DataFrame(columns=['name', 'Status'])
+
+list_0 = []
 
 path1 = "W:/Coding/PythonProjects/ScrappedData/OneDrive/Cars/" + str(ExcelName)
 path2 = "W:/Coding/PythonProjects/ScrappedData/OneDrive/Flats/" + str(ExcelName)
-path3 = "W:/Coding/PythonProjects/ScrappedData/OneDrive/Flats/Daugavpils/" + str(ExcelName)
+path3 = "W:\Coding\PythonProjects\ScrappedData\OneDrive\Daugavpils/" + str(ExcelName)
 path4 = "W:/Coding/PythonProjects/ScrappedData/OneDrive/Salidzini_scrapping/" + str(ExcelName)
-paths = [path1,path2,path3,path4]
+path5 = "W:/Coding/PythonProjects/ScrappedData/OneDrive/Flats_Riga_Region/" + str(ExcelName)
+paths = [path1,path2,path3,path4,path5]
+
 for path in paths:
     isExist = os.path.exists(path)
-    print(path)
-    print(isExist)
+    list_0.append(path)
+    list_0.append(isExist)
+send_email(str(list_0))
+print('Email sent out succesfully')
